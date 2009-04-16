aa0f4f8cf8b16e517eb9f4a48fc1ac51394c3339
jmostazo-upm <jmostazo-upm@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-04-16 11:22:59 +0000

    refs #327. Problem with BeautifulSoap in MediaWikiAdaptor
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@143 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

---
+++ TID/trunk/services/MediaWikiAdaptor/MediaWiki/utils.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import urllib, urllib2, httplib
+import urlparse
+
+def queryGET(url, params=None, headers={}):
+    proto, host, cgi = urlparse.urlparse(url)[:3]
+    conn = httplib.HTTPConnection(host)
+    print url
+    print params
+    if params:
+        conn.request ('GET', cgi +'?%s' % urllib.urlencode(params), headers)
+    else:
+        conn.request('GET', cgi, headers)
+    response = conn.getresponse()
+    return response
+
+
+
+def queryPOST(url, params=None, headers={}):
+    proto, host, cgi = urlparse.urlparse(url)[:3]
+    conn = httplib.HTTPConnection(host)
+
+    if params:
+        conn.request("POST", cgi, urllib.urlencode(params), headers)
+    else:
+        conn.request("POST", cgi, headers)
+
+    response = conn.getresponse()
+    return response
+
+
+def queryPUT(url, params=None, headers={}):
+    proto, host, cgi = urlparse.urlparse(url)[:3]
+    conn = httplib.HTTPConnection(host)
+
+    if params:
+        conn.request("PUT", cgi, urllib.urlencode(params), headers)
+    else:
+        conn.request("PUT", cgi, headers)
+
+    response = conn.getresponse()
+    return response
+
+
+def queryDELETE(url, params=None, headers={}):
+    proto, host, cgi = urlparse.urlparse(url)[:3]
+    conn = httplib.HTTPConnection(host)
+
+    if params:
+        conn.request("DELETE", cgi, urllib.urlencode(params), headers)
+    else:
+        conn.request("DELETE", cgi, headers)
+
+    response = conn.getresponse()
+    return response
+
+
+
---
+++ TID/trunk/services/MediaWikiAdaptor/MediaWiki/views.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from django.core.cache import cache
+from utils import queryGET
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from xmlParser import *
+from intExcept import HttpException
+import urllib
+
+def DESCRIPTION(request, url, value, sroffset, srlimit):
+# it will show the description for the article given
+	if request.method == 'GET':
+		# format will set the format desired
+		format = 'xml'
+		# params will set the parameters in the http request
+		params = {'action':'opensearch',
+			'search':value.encode('utf8'),
+			'format':format}
+		# Get the result of the request
+		response = queryGET(url, params, {})
+		result = response.read()
+		response.close()
+		
+		params = {'action' : 'query',
+			  'prop' : 'links',
+			  'pllimit' : 500,
+			  'sroffset': sroffset,
+			  'srlimit' : srlimit,
+			  'titles': value.encode('utf8'),
+			  'format': format}
+
+		response = queryGET(url, params, {})
+		result2 = response.read()
+		response.close()
+		try:
+			# Parsing the xml returned
+			result = parserDescription (result, result2, value)
+		except:
+			return HttpException("error.xml", {'code':404,
+							   'description':'Mediawiki not found'})
+		return render_to_response ('description.xml', result, mimetype='text/xml')
+	else:
+		return HttpException("error.xml", {'code':405,
+                			           'description':'Method not allowed: The method allowed is GET'})
+
+
+def SEARCH(request, url, value, sroffset, srlimit):
+    
+	if request.method == 'GET':
+		params = {'action' : 'query',
+			  'list' : 'search',
+			  'format' : 'xml',
+			  'sroffset': sroffset,
+			  'srlimit': srlimit,
+			  'srsearch' : value.encode('utf8')}
+        
+		# Get result of the request
+		response = queryGET(url, params, {})
+		# Parsing the results
+		result = response.read()
+		response.close()
+
+		try:
+			result = parserSearch(result)
+		except:
+			return HttpException("error.xml", {'code':404,
+							   'description':'Mediawiki not found'})
+
+		# Returning xml
+		return render_to_response("search.xml", {'pages':result}, mimetype="text/xml")
+	
+	else:
+		return HttpException("error.xml", {'code':405,
+						   'description':'Method not allowed: The method allowed is GET'})
+        
+
+
+def CONTENTARTICLE(request, url, value):
+
+	if request.method == 'GET':
+		cache._cache.clear() # Limpia la cache	
+		url = 'http://'+url+'.wikipedia.org/wiki/' + value
+		userAgent = "Mozilla/5.0 (X11; U; Linux i686; es-ES; rv:1.9.0.5) Gecko/2008121621 Ubuntu/8.04 (hardy) Firefox/3.0.3;"
+
+		# Get result of the request
+		headers = {"User-Agent" : userAgent}
+		response = queryGET(url, headers)
+
+		# Parsing the results
+		result = response.read()
+		response.close()
+		#try:
+		result = parserContentArticle(result)
+		#except:
+#		return HttpException("error.xml", {'code':404,
+#							   'description':'Wikipedia not found'})
+		return render_to_response("contentArticle.xml", {'content':result}, mimetype = "text/xml")
+	else:
+		return HttpException("error.xml", {'code':405,
+						   'description':'Method not allowed: The method allowed is GET'})
