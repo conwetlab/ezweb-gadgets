baf162618ed10566c3314cd4631f6081377c693f
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-04-16 11:35:55 +0000

    refs #327. changes in the upm gadgets
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@145 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- TID/trunk/BolsaGadget/js/BolsaGadgetLogic.js
+++ TID/trunk/BolsaGadget/js/BolsaGadgetLogic.js
@@ -13,11 +13,11 @@ function refresh(type, _symbols, _tags, indexTab)
 	{
 	    if (s != "")
 		{
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 		}
 	    else
 		{
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 		}
 	}
 	
@@ -69,9 +69,9 @@ function getNamesMarkets(_symbols)
 	for(var i=0; i < _symbols.length; i++)
 	{
 		if (s != "")
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 	    else
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 	}
 	
 	var t = "ns";
@@ -87,9 +87,9 @@ function getNamesEnterprises(list, value)
 	for(var i=0; i < value.length; i++)
 	{
 		if (s != "")
-		    s += "+" + encodeURIComponent("@"+value[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent("@"+value[i].replace(/ /g, ""));
 	    else
-		    s += "+" + encodeURIComponent("@"+value[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent("@"+value[i].replace(/ /g, ""));
 	}
 	
 	var t="ns";
@@ -106,9 +106,9 @@ function getNamesEnterprisesSelected(_symbols)
 	for(var i=0; i < _symbols.length; i++)
 	{
 		if (s != "")
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 	    else
-		    s += "+" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
+		    s += "%2B" + encodeURIComponent(_symbols[i].replace(/ /g, ""));
 	}
 	
 	var t="ns";
@@ -132,7 +132,7 @@ function getQuoteMarket(symbols, tags)
     for(var i = 0; i < symbols.length; i++)
 	{
 		if (s != "")
-			s += "+" + encodeURIComponent(symbols[i]);
+			s += "%2B" + encodeURIComponent(symbols[i]);
 		else
 			s += encodeURIComponent(symbols[i]);
     }
@@ -161,7 +161,7 @@ function getQuoteFile(symbols, tags)
     for(var i = 0; i < symbols.length; i++)
 	{
 		if (s != "")
-			s += "+" + encodeURIComponent(symbols[i].replace(/ /g, ""));
+			s += "%2B" + encodeURIComponent(symbols[i].replace(/ /g, ""));
 		else
 			s += encodeURIComponent(symbols[i].replace(/ /g, ""));
     }
@@ -191,7 +191,7 @@ function getQuoteEnterprise(symbols, tags)
     for(var i = 0; i < symbols.length; i++)
 	{
 		if (s != "")
-			s += "+" + encodeURIComponent(symbols[i].replace(/\^/g, "").replace(/@/g, ""));
+			s += "%2B" + encodeURIComponent(symbols[i].replace(/\^/g, "").replace(/@/g, ""));
 		else
 			s += encodeURIComponent(symbols[i].replace(/\^/g, "").replace(/@/g, ""));
     }
@@ -339,7 +339,7 @@ function onSuccessGetNameQuoteEnterpriseSelected(response)
 	displayNameEnterpriseSelected(quote);
 }
 
-function onError()
+function onError(transport, e)
 {
 	removeLoadingImage();
 }
