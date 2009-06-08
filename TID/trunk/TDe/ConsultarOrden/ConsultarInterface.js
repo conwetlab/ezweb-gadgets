4d0913b5130405e4c4383896e64da325c51d137a
jaimegc <jaimegc@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-06-08 10:51:24 +0000

    refs #273. Telefonica Deutschland gadgets
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@221 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- TID/trunk/TDe/ConsultarOrden/ConsultarInterface.js
+++ TID/trunk/TDe/ConsultarOrden/ConsultarInterface.js
@@ -13,114 +13,40 @@ var value = '';
 var informacion = new StyledElements.StyledNotebook({'id':'informacion'});
 
 var headers = ['Telefonica Subscriber Id',
-				'Reseller Subscriber Id',
 				'Reseller Order Id',
 				'Line Type',
 				'Current TAL Carrier',
 				'Status',
 				'Order Status',
-				'Fax out to DTAG',
-				'Fax DTAG in',
-				'Confirmed Date',
-				'Real Delivery Date',
 				'City',
 				'Street',
-				'Number',
-				'CP'];
+				'House number',
+				'Postal Code'];
+									
 				
-var datos = {'133670-20061003-ORD00016': ['3383',
-				'',
-				'ANAIS-HANSENET-60083',
-				'port-only',
+var datos = {'133670-20061115-OR001369': ['302095',
+				'1-FD1MZQ',
+				'full unbundled',
 				'T-com',
-				'IN PROCESS',
-				'orderConfirmation',
-				'',
-				'',
-				'03/10/2006',
-				'',
-				'Madrid',
-				'C/ Emilio Vargas',
-				'6',
-				'28080'],
-			'133670-20061003-ORD00006': ['2316',
-				'',
-				'ANAIS-HANSENET-12327',
-				'port-only',
-				'T-com',
-				'WAITING',
-				'orderConfirmation',
-				'',
-				'',
-				'21/05/2008',
-				'',
-				'Barcelona',
-				'Via Augusta',
-				'117',
-				'08021'],
-			'133670-20061003-ORD00005': ['4312',
-				'',
-				'ANAIS-HANSENET-12327',
-				'port-only',
-				'T-com',
-				'IN PROCESS',
-				'orderConfirmation',
-				'',
-				'',
-				'21/05/2008',
-				'',
-				'Madrid',
-				'Paseo Castellana',
-				'134',
-				'28082'],	
-			'133670-20061003-ORD00003': ['2515',
-				'',
-				'ANAIS-HANSENET-12327',
-				'port-only',
-				'T-com',
-				'IN PROCESS',
-				'orderConfirmation',
-				'',
-				'',
-				'21/05/2008',
-				'',
-				'Madrid',
-				'Paseo Castellana',
-				'134',
-				'28082'],		
-			'133670-20061003-ORD00002': ['1312',
-				'',
-				'ANAIS-HANSENET-12327',
-				'port-only',
-				'T-com',
-				'IN PROCESS',
-				'orderConfirmation',
-				'',
-				'',
-				'21/05/2008',
-				'',
-				'Madrid',
-				'Paseo Castellana',
-				'134',
-				'28082'],		
-			'133670-20060928-ORD00012': ['5273',
-				'',
-				'ANAIS-HANSENET-12327',
-				'port-only',
-				'T-com',
-				'IN PROCESS',
-				'orderConfirmation',
-				'',
-				'',
-				'22/03/2008',
-				'',
-				'Madrid',
-				'Paseo Castellana',
-				'134',
-				'28082'],		
-				};
-				
-				
+				'EXECUTED',
+				'orderExecution',
+				'Kiel',
+				'Verdieckstr.',
+				'35',
+				'24149'
+				],
+			'133670-20080515-CA000396':	['2536911',	'1-1I3L0P2',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Hochheim',	'Schwedenstr.',	'9',	'65239'],
+			'133670-20080516-CA000488':	['1988240',	'1-1I4HNUF',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Ludwigsburg',	'Neckarstr.',	'52',	'71640'],
+			'133670-20080528-CA000178':	['2607220',	'1-1INTIAB',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Rosenheim',	'Sterzinger Str.',	'25',	'83024'],
+			'133670-20080603-CA000778':	['1003062',	'1-1K3ER41',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Schriesheim',	'In der Schanz',	'43',	'69198'],
+			'133670-20080613-CA000908':	['2183012',	'1-1LRGKXZ',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Werne',	'Dietrich-Bonhoeffer-Str.',	'16',	'59368'],
+			'133670-20080804-CA000582':	['2507911',	'1-1RP0WZI',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Puttlingen',	'Professor-Hirschmann-Str.',	'53',	'66346'],
+			'133670-20090423-OR000299':	['3942242',	'1-2HGNIZA',	'full unbundled',	'T-Com',	'EXECUTED',	'lineActivation',	'Neukirchen-Adorf',	'Hauptstr.',	'94',	'9221'],
+			'133670-20090504-OR003791':	['3969339',	'1-2I9TNL9',	'full unbundled',	'T-Com',	'EXECUTED',	'lineActivation',	'Eichwalde',	'Lessingstr.',	'20',	'15732'],
+			'133670-20090526-OR002298':	['4022495',	'1-2JSSGUS',	'full unbundled',	'T-Com',	'IN PROCESS',	'orderConfirmation',	'Stade',	'Wilhelm-Raabe-Str.',	'14',	'21680'],
+			'133670-20090529-OR001996': ['4030799',	'1-2K230P7',	'full unbundled',	'T-Com', 	'IN PROCESS',	'orderConfirmation',	'Lutherstadt Wittenberg',	'Wiesigk',	'9',	'6886'],
+			'133670-20080415-CA000397':	['2496087',	'1-1E3X8Y4',	'full unbundled',	'T-Com',	'EXECUTED',	'terminationExecution',	'Taunusstein',	'Am Haferstuck', '14',	'65232']			
+}		
 
 var Consulta = function() {
     /* Call to the parent constructor */
@@ -143,7 +69,7 @@ Consulta.prototype.init = function() {
 	document.body.appendChild (content);
 	
 		
-	var textinfo = "<table align='center' cellspacing='2'>";
+	/*var textinfo = "<table align='center' cellspacing='2'>";
 	
 	for(i=0;i<headers.length;i++){
 		if(i%2==0){
@@ -157,13 +83,13 @@ Consulta.prototype.init = function() {
 	
 	var info = document.createElement('div');
 	info.setAttribute ('id', 'info');
-	info.innerHTML = textinfo;
+	info.innerHTML = textinfo;*/
 	
-	var tab1 = informacion.createTab ({'name':'133670-20061003-ORD00016'});
-	tab1.appendChild (info);
-	informacion.goToTab(tab1.getId());
+	//var tab1 = informacion.createTab ({'name':'133670-20061003-ORD00016'});
+	//tab1.appendChild (info);
+	//informacion.goToTab(tab1.getId());
 	informacion.insertInto(document.getElementById('content'));	
-	address.set(datos['133670-20061003-ORD00016'][11] + ", " + datos['133670-20061003-ORD00016'][12] + ", " + datos['133670-20061003-ORD00016'][13] + ", " + datos['133670-20061003-ORD00016'][14]);
+	//address.set(datos['133670-20061003-ORD00016'][11] + ", " + datos['133670-20061003-ORD00016'][12] + ", " + datos['133670-20061003-ORD00016'][13] + ", " + datos['133670-20061003-ORD00016'][14]);
 	
 }
 
@@ -171,6 +97,7 @@ Consulta.prototype.init = function() {
 Consulta = new Consulta();
 
 function orderId_handler (value){
+	
 	
 	var tab1 = informacion.createTab ({'name':value});
 
@@ -190,9 +117,10 @@ function orderId_handler (value){
 	info.innerHTML = textinfo;
 	
 	tab1.appendChild (info);
+	
 	informacion.goToTab(tab1.getId());
 	
-	address.set(datos[value][11] + ", " + datos[value][12] + " " + datos[value][13] + ", " + datos[value][14]); 
+	address.set(datos[value][6] + ", " + datos[value][7] + " " + datos[value][8] + ", " + datos[value][9]); 
 	
 	
 	
--- TID/trunk/TDe/OrdenesError/OrdenesErrorInterface.js
+++ TID/trunk/TDe/OrdenesError/OrdenesErrorInterface.js
@@ -11,78 +11,31 @@ var description = EzWebAPI.createRWGadgetVariable ('description');
 var value = '';
 var informacion = new StyledElements.StyledNotebook({'id':'informacion'});
 
+
+
 var headers = ['TD-Order-ID',
 				'Status',
 				'OrderStatus',
 				'Confirmed delivery date',
 				'Termination date',
 				'VoiceSubscriberID',
-				'IDeSubscriberID',
+				'TDeSubscriberID',
 				'Error date',
 				'Error code',
 				'Error description'];
-				
-var datos = [['133670-20061003-ORD00016',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'',
-				'3383',
-				'03/10/2006, 14:43:35',
-				'5020',
-				'Error sending message to voice system'],
-			['133670-20061003-ORD00006',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'',
-				'3373',
-				'03/10/2006, 13:00:02',
-				'5020',
-				'Error sending message to voice system'],
-			['133670-20061003-ORD00005',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'',
-				'3372',
-				'03/10/2006, 12:05:43',
-				'5020',
-				'Error sending message to voice system'],
-			['133670-20061003-ORD00003',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'',
-				'3372',
-				'03/10/2006, 12:05:43',
-				'5020',
-				'Error sending message to voice system'],
-			['133670-20061003-ORD00002',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'',
-				'3372',
-				'03/10/2006, 12:05:43',
-				'5020',
-				'Error sending message to voice system'],
-			['133670-20060928-ORD00012',
-				'IN PROCESS',
-				'orderConfirmation',
-				'03/10/2006, 02:00:00',
-				'',
-				'804275',
-				'3372',
-				'03/10/2006, 12:05:43',
-				'5020',
-				'Error sending message to voice system']	
-				];
+			
+var datos = [['133670-20090529-OR001996',	'IN PROCESS',	'orderConfirmation',	'27/07/2009.00:00:00', '',	'',		'4030799',	'05/06/2009.11:14:27',	'-1',	'Not accepted (5210) \'\''],
+['133670-20090526-OR002298',	'IN PROCESS',	'orderConfirmation',	'17/06/2009.00:00:00', '',		'9571387',	'4022495',	'04/06/2009.08:24:14',	'-1',	'Not accepted (42570) \'4257: Es liegt bereits ein Transfer-Auftrag (RNFlip) vor. (beginning: 4141660694, endnr: 4141660694, RNFlip count: 1)\''],
+['133670-20090504-OR003791',	'EXECUTED',	'lineActivation',	'14/05/2009.00:00:00',	'',	'9474267',	'3969339',	'26/05/2009.11:57:29',	'-1',	''],
+['133670-20090423-OR000299',	'EXECUTED',	'lineActivation',	'15/05/2009.00:00:00',	'',	'9474139',	'3942242',	'26/05/2009.11:55:34',	'-1',	''],
+['133670-20080804-CA000582',	'EXECUTED',	'terminationExecution',  '',	'',		'7314459', 	'2507911',	'23/02/2009.15:55:40',	'-1',	'Error updating subscriber (42240) \'4224: Kunde kann nicht deaktiviert werden, solange offene Portierungen existieren! (7314459: offene Portierungen: 1)\''],
+['133670-20080415-CA000397',	'EXECUTED',	'terminationExecution',  '',	'',		'7331105',	 '2496087',	'23/02/2009.15:06:08',	'-1',	'Error updating subscriber (42230) \'4223: Kunde kann nicht deaktiviert werden, solange Nummern aktiv sind! (7331105: aktive Nummern: 1)\''],
+['133670-20080613-CA000908',	'EXECUTED',	'terminationExecution',  '',	'',	'7146686',	'2183012',	'23/02/2009.15:04:10',	'-1',	'Error updating subscriber (42240) \'4224: Kunde kann nicht deaktiviert werden, solange offene Portierungen existieren! (7146686: offene Portierungen: 1)\''],
+['133670-20080515-CA000396',	'EXECUTED',	'terminationExecution',  '',	'',		'7340799',	'2536911',	'23/02/2009.15:03:46',	'-1',	'Error updating subscriber (42230) \'4223: Kunde kann nicht deaktiviert werden, solange Nummern aktiv sind! (7340799: aktive Nummern: 2)\''],
+['133670-20080516-CA000488',	'EXECUTED',	'terminationExecution',	'',	'',	'6894437',	'1988240',	'23/02/2009.15:03:06',	'-1',	'Error updating subscriber (42230) \'4223: Kunde kann nicht deaktiviert werden, solange Nummern aktiv sind! (6894437: aktive Nummern: 2)\''],
+['133670-20080603-CA000778',	'EXECUTED',	'terminationExecution',	'',	'',	'6790204',	'1003062',	'23/02/2009.15:02:37',	'-1',	'Error updating subscriber (42240) \'4224: Kunde kann nicht deaktiviert werden, solange offene Portierungen existieren! (6790204: offene Portierungen: 1)\''],
+['133670-20080528-CA000178',	'EXECUTED',	'terminationExecution',	'',	'',	'7377793',	'2607220',	'23/02/2009.15:02:36',	'-1',	'Error updating subscriber (42230) \'4223: Kunde kann nicht deaktiviert werden, solange Nummern aktiv sind! (7377793: aktive Nummern: 1)\'']
+];
 
 var Ordenes = function() {
     /* Call to the parent constructor */
@@ -121,7 +74,7 @@ Ordenes.prototype.init = function() {
 				
 		for(i=0;i<headers.length;i++){
 				if(i==0){
-					textinfo += "<td><span class='send' onclick='sendEvent(\""+datos[j][i]+"\",\""+datos[j][9]+"\")'>"+datos[j][i]+"<span></td>";	
+					textinfo += "<td><span class='send' onclick='sendEvent(\""+datos[j][i]+"\","+j+")'>"+datos[j][i]+"<span></td>";	
 				}else{
 					textinfo += "<td>"+datos[j][i]+"</td>";
 				}
@@ -145,7 +98,8 @@ Ordenes = new Ordenes();
 
 function sendEvent(id,desc){
 	orderId.set(id);
-	description.set(desc);
+
+	description.set(datos[desc][0]+": "+datos[desc][9]);
 }
 
 /*************************************************************************/
