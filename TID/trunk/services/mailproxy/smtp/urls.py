95ae06c9a588d839640386c1fb24c7184406e7b4
jmostazo-upm <jmostazo-upm@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-08-04 09:54:39 +0000

    refs #327 deleting mailproxy
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@446 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- TID/trunk/services/mailproxy/smtp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns
-from smtp.views import *
-
-urlpatterns = patterns('smtp.views',
-
-    # Send mail
-    (r'^/sender[/]?$', 
-        SendMail(permitted_methods=('POST',))),
-)
