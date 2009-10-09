5d26f243a66632dbb1dcdeab9c8a2e75098d4d39
pjm <pjm@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-10-09 12:52:29 +0000

    Cambios en servicio Open Movilforum MMS
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@630 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- TID/trunk/services/openmovilforum/mms/MMSSenderAPI.py
+++ TID/trunk/services/openmovilforum/mms/MMSSenderAPI.py
@@ -1,14 +1,14 @@
 # (C) Copyright 2009 Telefonica Investigacion y Desarrollo S.A.Unipersonal (Telefonica I+D)
 # Todos los derechos reservados
-# Basada en la librería MMSSenderAPI.py de Open Movilforum: open@open.movilforum.com
+# Basada en la libreria MMSSenderAPI.py de Open Movilforum: open@open.movilforum.com
 
-# Este programa es software libre: usted puede redistribuirlo y/o modificarlo bajo los términos 
-# de la Licencia Pública General GNU publicada por la Fundación para el Software Libre, ya sea la
-# versión 3 de la Licencia, o (a su elección) cualquier versión posterior.
-# Este programa se distribuye con la esperanza de que sea útil, pero SIN GARANTÍA ALGUNA; ni siquiera
-# la garantía implícita MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. Consulte los detalles
-# de la Licencia Pública General GNU para obtener una información más detallada. Debería haber recibido
-# una copia de la Licencia Pública General GNU junto a este programa. En caso contrario, 
+# Este programa es software libre: usted puede redistribuirlo y/o modificarlo bajo los terminos 
+# de la Licencia Publica General GNU publicada por la Fundacion para el Software Libre, ya sea la
+# version 3 de la Licencia, o (a su eleccion) cualquier version posterior.
+# Este programa se distribuye con la esperanza de que sea util, pero SIN GARANTÍA ALGUNA; ni siquiera
+# la garantia implicita MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. Consulte los detalles
+# de la Licencia Publica General GNU para obtener una informacion mas detallada. Deberia haber recibido
+# una copia de la Licencia Publica General GNU junto a este programa. En caso contrario, 
 # consulte <http://www.gnu.org/licenses/>.
 
 import httplib, urllib, urlparse
