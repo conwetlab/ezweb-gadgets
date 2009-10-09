784137e0bfd3712a0be1b469d61e3623524be46c
pjm <pjm@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2009-10-09 12:54:41 +0000

    Cambios en servicio Open Movilforum MMS
    git-svn-id: file:///home/borja/svnDUMPS/ezweb-gadgets@631 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- TID/trunk/services/openmovilforum/mms/MMSSenderAPI.py
+++ TID/trunk/services/openmovilforum/mms/MMSSenderAPI.py
@@ -5,8 +5,8 @@
 # Este programa es software libre: usted puede redistribuirlo y/o modificarlo bajo los terminos 
 # de la Licencia Publica General GNU publicada por la Fundacion para el Software Libre, ya sea la
 # version 3 de la Licencia, o (a su eleccion) cualquier version posterior.
-# Este programa se distribuye con la esperanza de que sea util, pero SIN GARANTÍA ALGUNA; ni siquiera
-# la garantia implicita MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. Consulte los detalles
+# Este programa se distribuye con la esperanza de que sea util, pero SIN GARANTIA ALGUNA; ni siquiera
+# la garantia implicita MERCANTIL o de APTITUD PARA UN PROPOSITO DETERMINADO. Consulte los detalles
 # de la Licencia Publica General GNU para obtener una informacion mas detallada. Deberia haber recibido
 # una copia de la Licencia Publica General GNU junto a este programa. En caso contrario, 
 # consulte <http://www.gnu.org/licenses/>.
